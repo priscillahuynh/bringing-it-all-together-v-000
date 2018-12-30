22b478c72c697498045a99acbcb78cf4d569a844
Priscilla H <priscillamhuynh@gmail.com>
2018-12-30 06:25:02 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -58,7 +58,8 @@ class Dog
     self.new(id:row[0], name:row[1],breed:row[2])
   end
 
-  def self.find_or_create_by
-
+  def self.find_or_create_by(name:,breed:)
+    dog = DB[:conn].execute("SELECT * FROM songs WHERE name = ?")
+    
   end
 end
