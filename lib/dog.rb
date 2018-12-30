fdecdc6f76c53524933325f6673c1ecdb00405b8
Priscilla H <priscillamhuynh@gmail.com>
2018-12-30 05:55:29 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -54,7 +54,10 @@ class Dog
     end.first
   end
 
-  def self.new_from_db
-    
-  end 
+  def self.new_from_db(row)
+    new_dog = Dog.new 
+    new_dog.id = row[0]
+    new_dog.name = row[1]
+    new_dog.breed = row[2]
+  end
 end
