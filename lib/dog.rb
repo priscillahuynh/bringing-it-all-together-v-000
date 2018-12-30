bf5c78a7ccc2bef3dbcd35c12f9bd4eff3b5d6af
Priscilla H <priscillamhuynh@gmail.com>
2018-12-30 06:48:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,8 @@
 class Dog
   attr_accessor :id, :name, :breed
 
-  def initialize(id: nil, name:, breed:)
-    @id = id
-    @name = name
-    @breed = breed
+  def initialize(details)
+    details.each {|attr, value| self.send("#{attr}=", value)}
   end
 
   def self.create_table
