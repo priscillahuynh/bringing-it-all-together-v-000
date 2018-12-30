1d02f031b68146f800bc13d843eaacd1cf3e8f39
Priscilla H <priscillamhuynh@gmail.com>
2018-12-30 05:53:23 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -42,12 +42,12 @@ class Dog
   end
 
   def self.find_by_id(id)
-    sql = <<-SQL 
+    sql = <<-SQL
     SELECT *
     FROM dogs
     WHERE id = ?
     LIMIT 1
-    SQL 
+    SQL
 
     DB[:conn].execute(sql,id).map do |row|
     self.new_from_db(row)
