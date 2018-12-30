bf147d9e83eb159a9274f7162c4f45601f6d0a77
Priscilla H <priscillamhuynh@gmail.com>
2018-12-30 06:22:33 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -55,10 +55,7 @@ class Dog
   end
 
   def self.new_from_db(row)
-    self.new.tap do |x|
-      id = row[0]
-      name = row[1]
-      breed = row[2]
+    self.new(id:row[0], name:row[1],breed:row[2])
     end
   end
 end
