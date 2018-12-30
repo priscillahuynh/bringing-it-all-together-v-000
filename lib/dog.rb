e4c7d9a401c50aeb60819e33a49e07f3f5777e5f
Priscilla H <priscillamhuynh@gmail.com>
2018-12-30 06:26:28 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -62,7 +62,9 @@ class Dog
     dog = DB[:conn].execute("SELECT * FROM songs WHERE name = ? and breed = ?", name, album)
     if !dog.empty?
       dog_data = dog[0]
-      dog = dog_data[0],dog_data[1],dog_data[2]
+      dog = dog_data[0], dog_data[1], dog_data[2]
+    else
+      dog = self.create(name: name, breed: breed)
 
   end
 end
